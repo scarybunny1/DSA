791accff72d5845a6f0d3588168e5931d529894c
Ayush Bhatt <46290395+scarybunny1@users.noreply.github.com>
2022-10-28 10:52:13 +0530

    Time: 159 ms (74.21%), Space: 17.9 MB (58.95%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,5 +1,5 @@
 class Solution:
-    def getAnagramWords(self, arr):
+    def getAnagramWordDictionary(self, arr):
         d = defaultdict(list)
         
         for string in arr:
@@ -8,7 +8,20 @@ class Solution:
         
         return d
     
+    def getAnagramWordDictionary2(self, arr):
+        d = defaultdict(list)
+        
+        for string in arr:
+            s = [0]*26
+            for char in string:
+                index = ord(char) - ord('a')
+                s[index] += 1
+            hashable_key = "#".join(map(str, s))
+            d[hashable_key].append(string)
+            
+        return d
+    
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        d = self.getAnagramWords(strs)
+        d = self.getAnagramWordDictionary2(strs)
         
         return [l for l in d.values()]
