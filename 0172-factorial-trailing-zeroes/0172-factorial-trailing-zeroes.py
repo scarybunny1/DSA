93754240f4c6f379232094185b50ab8fd3826ac2
Ayush Bhatt <46290395+scarybunny1@users.noreply.github.com>
2022-10-28 22:15:37 +0530

    Time: 761 ms (23.70%), Space: 13.9 MB (22.28%) - LeetHub

---
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.py
@@ -0,0 +1,21 @@
+class Solution:
+    def getZeros(self, n, two, five, ten):
+        while n % 10 == 0:
+            ten += 1
+            n = n // 10
+        
+        while n % 5 == 0:
+            five += 1
+            n = n // 5
+        
+        while n % 2 == 0:
+            two += 1
+            n = n // 2
+        
+        return (two, five, ten)
+    
+    def trailingZeroes(self, n: int) -> int:
+        two = five = ten = 0
+        for num in range(2, n+1):
+            two, five, ten = self.getZeros(num, two, five, ten)
+        return ten + min(two, five)
