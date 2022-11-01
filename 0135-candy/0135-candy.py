cd9e8e89df7678bf411122b9271f6e5bf7b98387
Ayush Bhatt <46290395+scarybunny1@users.noreply.github.com>
2022-11-01 12:33:31 +0530

    Time: 522 ms (11.81%), Space: 16.8 MB (76.76%) - LeetHub

--- 0135-candy/0135-candy.py
+++ 0135-candy/0135-candy.py
@@ -1,17 +1,25 @@
 class Solution:
     def candy(self, ratings: List[int]) -> int:
         
-        # 3 3 2 1 4 8 2 1
-        # 0 1 2 3 4 5 6 7
+        # ratings -     3 3 2 1 4 8 2 1
+        # index -       0 1 2 3 4 5 6 7
+        # left -        1 1 1 1 2 3 1 1
+        # right -       1 3 2 1 1 3 2 1
+        # ans -         1 3 2 1 2 3 2 1
+        # sum - 1+3+2+1+2+3+2+1 = 15
+        
+        # 0 -> n-1 - 0
+        # 1 -> n-1 - 1
+        # 2 -> n-1 - 2
         
         n = len(ratings)
         left, right = [1] * n, [1] * n
         for i in range(1, n):
             if ratings[i] > ratings[i-1]:
                 left[i] = left[i-1] + 1
-        
-        for i in range(n-2, -1, -1):
-            if ratings[i] > ratings[i+1]:
-                right[i] = right[i+1] + 1
+            
+            j = n - 1 - i
+            if ratings[j] > ratings[j+1]:
+                right[j] = right[j+1] + 1
                 
         return sum(max(left[i], right[i]) for i in range(n))
