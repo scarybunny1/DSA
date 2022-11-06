cd7549d162e2b4ba59fb0787368905c56d25cc7a
Ayush Bhatt <46290395+scarybunny1@users.noreply.github.com>
2022-11-06 12:50:42 +0530

    Time: 68 ms (23.81%), Space: 13.8 MB (98.81%) - LeetHub

--- 0899-orderly-queue/0899-orderly-queue.py
+++ 0899-orderly-queue/0899-orderly-queue.py
@@ -1,10 +1,6 @@
 class Solution:
     def orderlyQueue(self, s: str, k: int) -> str:
         if k == 1:
-            order = []
-            for i in range(len(s)):
-                new_string = s[i:] + s[:i]
-                order.append(new_string)
-            return sorted(order)[0]
+            return min(s[i:] + s[:i] for i in range(len(s)))
         else:
             return "".join(sorted(s))
