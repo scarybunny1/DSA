4899619fabd8013b9c01e6d0d07830158449ae6c
Ayush Bhatt <46290395+scarybunny1@users.noreply.github.com>
2022-12-25 16:26:37 +0530

    Time: 114 ms (91.55%), Space: 14.2 MB (79.61%) - LeetHub

--- 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.py
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.py
@@ -1,16 +1,38 @@
 class Solution:
     def answerQueries(self, nums: List[int], queries: List[int]) -> List[int]:
+        # O(n) solution
+        # res = []
+        # nums.sort()
+        # for total in queries:
+        #     left = 0
+        #     max_len = 0
+        #     curr_sum = 0
+        #     for right in range(len(nums)):
+        #         curr_sum += nums[right]
+        #         while curr_sum > total:
+        #             curr_sum -= nums[left]
+        #             left += 1
+        #         max_len = max(max_len, right - left + 1)
+        #     res.append(max_len)
+        # return res
+    
+        #O(logn) solution using Binary search
         res = []
         nums.sort()
+        arr = list(accumulate(nums))
         for total in queries:
-            left = 0
-            max_len = 0
-            curr_sum = 0
-            for right in range(len(nums)):
-                curr_sum += nums[right]
-                while curr_sum > total:
-                    curr_sum -= nums[left]
-                    left += 1
-                max_len = max(max_len, right - left + 1)
-            res.append(max_len)
+            ans = self.search(arr, total)
+            res.append(ans + 1)
         return res
+    
+    def search(self, nums, total):
+        left, right = 0, len(nums)-1
+        ans = -1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] <= total:
+                ans = mid
+                left = mid + 1
+            else:
+                right = mid - 1
+        return ans
