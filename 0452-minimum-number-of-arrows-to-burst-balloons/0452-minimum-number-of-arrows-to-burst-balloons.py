7a31941417e97249f2b16c33374ca2b8bb554d59
Ayush Bhatt <46290395+scarybunny1@users.noreply.github.com>
2023-01-03 14:57:36 +0530

    Time: 1554 ms (62.81%), Space: 60.3 MB (7.07%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.py
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.py
@@ -6,6 +6,16 @@ class Solution:
         #                                     --------------------------
         #                         ----------------------
         
+        points.sort(key= lambda x: x[1])
+        curr_end = points[0][1]
+        arrows = 1
+        for i in range(1, len(points)):
+            if points[i][0] > curr_end:
+                arrows += 1
+                curr_end = points[i][1]
+        return arrows
+            
+        
         def overlaps(i1, i2):
             return min(i1[1], i2[1]) - max(i1[0], i2[0]) >= 0
                 
@@ -21,3 +31,5 @@ class Solution:
                 stack.append(point)
         
         return len(stack)
+    
+        
