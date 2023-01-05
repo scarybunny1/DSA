2cde46aea69b88abd03a994c04ed304cc9abcae8
Ayush Bhatt <46290395+scarybunny1@users.noreply.github.com>
2023-01-05 16:15:44 +0530

    Time: 3329 ms (45.67%), Space: 53.7 MB (63.18%) - LeetHub

--- 2509-cycle-length-queries-in-a-tree/2509-cycle-length-queries-in-a-tree.py
+++ 2509-cycle-length-queries-in-a-tree/2509-cycle-length-queries-in-a-tree.py
@@ -1,14 +1,14 @@
 class Solution:
-    def getDist(self, x, y):
-        if x == y:
-            return 0
-        elif x > y:
-            return 1 + self.getDist(x//2, y)
-        else:
-            return 1 + self.getDist(x, y//2)
-        
     def cycleLengthQueries(self, n: int, queries: List[List[int]]) -> List[int]:
         ans = []
         for x, y in queries:
-            ans.append(1 + self.getDist(x, y))
+            path = 0
+            while x != y:
+                if x > y:
+                    x //= 2
+                    path += 1
+                else:
+                    y //= 2
+                    path += 1
+            ans.append(path+1)
         return ans
