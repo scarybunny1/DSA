184d6fdd8cfb97105e38fe85bb730083980aae08
Ayush Bhatt <46290395+scarybunny1@users.noreply.github.com>
2023-01-13 15:08:27 +0530

    Time: 1826 ms (84.21%), Space: 159.2 MB (26.11%) - LeetHub

---
+++ 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.py
@@ -0,0 +1,30 @@
+class Solution:
+    def longestPath(self, parent: List[int], s: str) -> int:
+        #01:48:00
+        #Input: Tree, Root - 0, Nodes - n, Array parent - parent[i] is parent of node i, String s - s[i] is character assigned to i
+        
+        #Output: length of longest path, Condition - adjacent nodes on same path do not have same character
+        n = len(parent)
+        children = defaultdict(list)
+        for i in range(1, n):
+            children[parent[i]].append(i)
+        self.longestPath = 1
+        return max(self.getLongestChain(0, s, children), self.longestPath)
+        
+    def getLongestChain(self, node, s, children):
+        longest_chain = 1    #Single Node
+        pq = []
+        for child in children[node]:
+            child_chain = self.getLongestChain(child, s, children)
+            if s[child] != s[node]:
+                heapq.heappush(pq, child_chain)
+                if len(pq) > 2:
+                    heapq.heappop(pq)
+        
+        if len(pq) == 2:
+            longest_chain = max(pq[0], pq[1]) + 1
+            self.longestPath = max(self.longestPath, pq[0] + pq[1] + 1)
+        elif len(pq) == 1:
+            longest_chain = pq[0] + 1
+            self.longestPath = max(self.longestPath, pq[0] + 1)
+        return longest_chain
