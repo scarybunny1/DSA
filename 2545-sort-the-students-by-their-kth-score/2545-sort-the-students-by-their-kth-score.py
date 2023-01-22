e20dac7b0ab24ce1bae854af054c23a9064765f5
Ayush Bhatt <46290395+scarybunny1@users.noreply.github.com>
2023-01-22 11:40:53 +0530

    Time: 439 ms (94.12%), Space: 20 MB (76.47%) - LeetHub

--- 2545-sort-the-students-by-their-kth-score/2545-sort-the-students-by-their-kth-score.py
+++ 2545-sort-the-students-by-their-kth-score/2545-sort-the-students-by-their-kth-score.py
@@ -1,4 +1,13 @@
 class Solution:
     def sortTheStudents(self, score: List[List[int]], k: int) -> List[List[int]]:
-        score.sort(reverse=True, key= lambda x: x[k])
-        return score
+        # score.sort(reverse=True, key= lambda x: x[k])
+        # return score
+    
+        arr = []
+        for i, l in enumerate(score):
+            arr.append([l[k], i])
+        arr.sort(reverse=True)
+        ans = []
+        for _, i in arr:
+            ans.append(score[i])
+        return ans
