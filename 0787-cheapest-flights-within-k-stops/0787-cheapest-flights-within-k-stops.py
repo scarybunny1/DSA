a9e951df9c7ce9f58ab06c27d41e9318f04a2e32
Ayush Bhatt <46290395+scarybunny1@users.noreply.github.com>
2023-01-26 12:40:38 +0530

    Time: 266 ms (36.64%), Space: 15.2 MB (54.38%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
@@ -1,21 +1,38 @@
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+#         graph = defaultdict(list)
+#         for x, y, price in flights:
+#             graph[x].append((y, price))
+        
+#         pq = [(0, -1, src)]
+#         s = [inf]*n
+        
+#         while pq:
+#             price, stops, city = heappop(pq)
+            
+#             if stops > k or stops >= s[city]:
+#                 continue
+#             if city == dst:
+#                 return price
+            
+#             s[city] = stops
+#             for neighbor, cost in graph[city]:
+#                 heappush(pq, (price + cost, stops + 1, neighbor))
+#         return -1
+    
         graph = defaultdict(list)
         for x, y, price in flights:
             graph[x].append((y, price))
         
-        pq = [(0, -1, src)]
-        s = [inf]*n
+        q = collections.deque([(-1, 0, src)])
+        costs = [inf] * n
         
-        while pq:
-            price, stops, city = heappop(pq)
-            
-            if stops > k or stops >= s[city]:
+        while q:
+            stops, cost, city = q.popleft()
+            if stops == k:
                 continue
-            if city == dst:
-                return price
-            
-            s[city] = stops
-            for neighbor, cost in graph[city]:
-                heappush(pq, (price + cost, stops + 1, neighbor))
-        return -1
+            for nei, price in graph[city]:
+                if cost + price < costs[nei]:
+                    costs[nei] = cost + price
+                    q.append((stops+1, cost+price, nei))
+        return -1 if costs[dst]==inf else costs[dst]
