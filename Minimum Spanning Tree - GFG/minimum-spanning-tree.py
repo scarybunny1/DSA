bc1bc846ed447254af7b8762a3c806902853d63b
Ayush Bhatt <46290395+scarybunny1@users.noreply.github.com>
2023-04-09 20:53:37 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.py
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.py
@@ -1,88 +1,25 @@
 #User function Template for python3
 import heapq
 class Solution:
-    def find(self, x, root):
-        if x == root[x]:
-            return x
-        root[x] = self.find(root[x], root)
-        return root[x]
-    
-    def union(self, x, y, root, rank):
-        rootX = self.find(x, root)
-        rootY = self.find(y, root)
-        
-        if rootX != rootY:
-            if rank[rootX] > rank[rootY]:
-                root[rootY] = rootX
-            elif rank[rootY] > rank[rootX]:
-                root[rootX] = rootY
-            else:
-                root[rootY] = rootX
-                rank[rootX] += 1
     
     #Function to find sum of weights of edges of the Minimum Spanning Tree.
-    def spanningTree(self, V, adj):
+    def spanningTree(self, V, graph):
         #code here
-        # visited = set()
-        
-        # mst = []
-        # mst_sum = 0
-        
-        # heap = [(0, 0, -1)]
-        ##O(E)
-        # while heap:
-        #     weight, node, parent = heapq.heappop(heap) ##O(log(E))
-        #     if node in visited:
-        #         continue
-        #     visited.add(node)
-        #     mst_sum += weight
-        #     if parent != -1:
-        #         mst.append((node, parent))
-                
-        #     for neighbor, edge_weight in adj[node]: 
-        #         heapq.heappush(heap, (edge_weight, neighbor, node))
-
-        # return mst_sum
-        
-        root = {i:i for i in range(V)}
-        rank = [0]*V
-        
-        edges = []
-        for node, neighbor_list in enumerate(adj):
-            for neighbor, weight in neighbor_list:
-                edges.append([weight, node, neighbor])
-        edges.sort()
-        
         mst_sum = 0
-        mst = []
-        for edge in edges:
-            edge_weight, node, neighbor = edge[0], edge[1], edge[2]
-            rootX = self.find(node, root)
-            rootY = self.find(neighbor, root)
-            
-            if rootX != rootY:
-                if rank[rootX] > rank[rootY]:
-                    root[rootY] = rootX
-                elif rank[rootY] > rank[rootX]:
-                    root[rootX] = rootY
-                else:
-                    root[rootY] = rootX
-                    rank[rootX] += 1
-                mst.append(edge)
-                mst_sum += edge_weight
+        visited = set()
         
+        pq = [(0, 0)]
+        while pq:
+            weight, node = heapq.heappop(pq)
+            if node in visited:
+                continue
+            visited.add(node)
+            mst_sum += weight
+            for nei, w in graph[node]:
+                heapq.heappush(pq, (w, nei))
         return mst_sum
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+
+
 #{ 
  # Driver Code Starts
 #Initial Template for Python 3
